97eb5a6683e3e791a9c7ea8600baf4d6e99622d7
martinGM05 <mtn.gm05@gmail.com>
2021-07-02 23:52:36 -0500

    Proyecto terminado

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ import           Data.Aeson.Lens                ( key, _Integer, _String, _Array
 import qualified Data.ByteString.Char8         as BS
 import           Data.Text                      ( Text )
 import qualified Data.Text.IO                  as TIO
+
 -- Importaciones además, agregar vector, aeson y unordered-containers en el archivo package.yaml
 import Data.Aeson
 import Data.Vector as V
@@ -26,27 +27,13 @@ getTemperature = preview (key "current" . key "temperature" . _Integer)
 
 getJSONPlace :: String -> IO BS.ByteString
 getJSONPlace place = do
-  let urlWithPlace = ("https://api.mapbox.com/geocoding/v5/mapbox.places/"Prelude.++ place Prelude.++".json?access_token=pk.eyJ1IjoibWFydGluLWdvbnphbGV6IiwiYSI6ImNrcWR6amV3bTBheDkyb21sN2hjY254d3gifQ.-nVVQYwdS8-jkUTG2rmbDg&limit=5&language=es")
+  let urlWithPlace = ("http://143.198.126.142:3000/api/v1/?place=" Prelude.++ place)
   request <- parseRequest urlWithPlace
   resPlace <- httpBS request
   return (getResponseBody resPlace)
 
-getPlace :: BS.ByteString -> Maybe Value
-getPlace = preview (key "features")
-
---getAnimes :: BS.ByteString -> [Vector Text]
-getAnimes bs = do
-  case eitherDecodeStrict' bs of
-    Left e -> error e
-    Right (Array array) -> do
-      V.forM array $ \v ->
-        case v of
-          Object o ->
-            case HashMap.lookup "place_name" o of
-              Nothing -> error "Didn't find color key"
-              Just (String c) -> return c
-              Just v' -> error $ "Expected a String, got: " Prelude.++ show v'
-          _ -> error $ "Expected an object, got: " Prelude.++ show v
+getPlace :: BS.ByteString -> Maybe Text
+getPlace = preview (key "places" . _String)
 
 
 main :: IO ()
@@ -57,18 +44,19 @@ main = do
   putStrLn "-------------------------------------------------------------------------------------------------------------------"
   putStrLn "-------------------------------------------------------------------------------------------------------------------"
   putStrLn "> "
-  putStr "> "
+  putStr ">> "
   place <- getLine
-  json <- getJSONTemperature place
+  json2 <- getJSONPlace place
 
-  json2 <- getJSONPlace place -- Obtengo mi json que tiene un arreglo con el arreg
-  let a = getPlace json2  -- obtengo el arreglo
-  print(getAnimes a)  -- se manda al metodo la variable que contiene el arreglo
+  case getPlace json2 of
+    Nothing -> TIO.putStrLn "No se encontro ninguna coincidencia con el lugar buscado"
+    Just places -> putStrLn(show places)
 
-  -- case getPlace json2 of
-  --   Nothing -> TIO.putStrLn "No se encontro ninguna coincidencia con el lugar buscado"
-  --   Just features -> putStrLn(""++ show features)
+  putStrLn ">> Ingrese un lugar de la lista"
+  ciudad <- getLine
+  json <- getJSONTemperature place 
+
 
   case getTemperature json of
     Nothing -> TIO.putStr "No se pudo obtener la temperatura"
-    Just temperature -> putStr("La temperatura en " Prelude.++ place Prelude.++ " es de : " Prelude.++ show temperature)
+    Just temperature -> putStr("La temperatura en " Prelude.++ ciudad Prelude.++ " es de : " Prelude.++ show temperature Prelude.++ " grados centígrados.")
